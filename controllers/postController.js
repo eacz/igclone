9d5aae0194a179dd584f1ea2785ae7680863e643
Esteban <estebanacanteros@gmail.com>
2020-12-07 18:21:47 -0300

    get a logged user's posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,3 +14,24 @@ module.exports.createPost = async (req, res) => {
         return res.status(500).json({ msg: error.message });
     }
 };
+
+module.exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find({}).populate('postedBy', '-password');
+        return res.json({ posts });
+    } catch (error) {
+        console.log(error);
+        return res.json({ msg: error.message });
+    }
+};
+
+module.exports.getUserPosts = async (req, res) => {
+    const user = req.user;
+    try {
+        const posts = await Post.find({ postedBy: user });
+        return res.json({ posts });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: error.message });
+    }
+};
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,7 @@ const postController = require('../controllers/postController');
 const verifyToken = require('../middlewares/authToken');
 
 router.post('/', verifyToken, postController.createPost);
+router.get('/all', verifyToken, postController.getAllPosts);
+router.get('/user', verifyToken, postController.getUserPosts);
 
 module.exports = router;
