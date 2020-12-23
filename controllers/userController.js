2e658bbef128db1abb6e66ffdf8da6840ef839a7
eacz <estebanacanteros@gmail.com>
2020-12-23 17:48:48 -0300

    search endpoint a bit improved

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,12 +10,14 @@ module.exports.getUserWithPosts = async (req, res) => {
         if (!user) {
             throw new Error("This user doesn't exists");
         }
-        const posts = await Post.find({ postedBy: user._id }).populate({
-            path: 'comments',
-            select: 'postedBy comment',
-            model: Comment,
-            populate: { path: 'postedBy', select: 'username photo' },
-        }).sort('-created')
+        const posts = await Post.find({ postedBy: user._id })
+            .populate({
+                path: 'comments',
+                select: 'postedBy comment',
+                model: Comment,
+                populate: { path: 'postedBy', select: 'username photo' },
+            })
+            .sort('-created');
         return res.json({ user, posts });
     } catch (error) {
         console.log(error);
@@ -56,13 +58,14 @@ module.exports.FollowUnfollow = async (req, res) => {
             user.following.includes(userID)
                 ? null
                 : (user.following.push(userID),
-                  userToInteract.followers.push(user._id))
+                  userToInteract.followers.push(user._id));
         } else {
             user.following = user.following.filter((userF) =>
                 userF.toString() === userID ? null : userF
             );
             userToInteract.followers = userToInteract.followers.filter(
-                (userF) => (userF.toString() === user._id.toString() ? null : userF)
+                (userF) =>
+                    userF.toString() === user._id.toString() ? null : userF
             );
         }
         await user.save();
@@ -76,27 +79,39 @@ module.exports.FollowUnfollow = async (req, res) => {
     }
 };
 
-module.exports.getListDetails = async (req,res) => {
-    const {usersIDS} = req.body
+module.exports.getListDetails = async (req, res) => {
+    const { usersIDS } = req.body;
     console.log(req);
     try {
-        const users = await User.find({_id: usersIDS}).select('username photo')
-        return res.json({users});
+        const users = await User.find({ _id: usersIDS }).select(
+            'username photo'
+        );
+        return res.json({ users });
     } catch (error) {
         console.log(error);
-        res.status(500)
-        return res.json({msg: error.message})
+        res.status(500);
+        return res.json({ msg: error.message });
     }
-}
+};
 
-
-module.exports.searchUsers = async (req,res) => {
-    const {search} = req.params;
+module.exports.searchUsers = async (req, res) => {
+    const { search } = req.params;
     try {
-        const users  = await User.find({username: search});
-        return res.json({users})
+        const users = await User.find({}).select('username photo name');
+        const selectedUsers = [];
+        
+        users.forEach((user) => {
+            if(user.username.toLowerCase().indexOf(search.toLowerCase()) !== -1){
+                selectedUsers.push(user)
+            }
+            if(user.name.toLowerCase().indexOf(search.toLowerCase()) !== -1){
+                selectedUsers.push(user)
+            }
+                
+        });
+        return res.json({ users: selectedUsers });
     } catch (error) {
         res.status(500);
-        return res.json({msg: error.message})
+        return res.json({ msg: error.message });
     }
-}
+};
